57fa950e82611d730e8dee75d7913ac6a2452f17
Ruth <rtilahun@brynmawr.edu>
2024-09-04 21:25:16 -0400

    Finished HelloUnity

--- HelloUnity/Assets/Scripts/HelloUnity.cs
+++ HelloUnity/Assets/Scripts/HelloUnity.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class HelloUnity : MonoBehaviour
 {
+    public float updateInterval = 0.5F;
+    private double lastInterval;
+    private int frames;
+    private float fps;
     // Start is called before the first frame update
     void Start()
     {
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
         Debug.Log("Hello Unity");
+    }
+    
+    void OnGUI()
+    {
+        GUILayout.Label("" + fps.ToString("f2"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ++frames;
+        float timeNow = Time.realtimeSinceStartup;
+        if (timeNow > lastInterval + updateInterval)
+        {
+            fps = (float)(frames / (timeNow - lastInterval));
+            frames = 0;
+            lastInterval = timeNow;
+            Debug.Log("Elapsed time: " + fps);
+        }
     }
 }
